a8cb424591d22e8c9c05b3a1cb7df0edccad7869
Richa Srivastava <84867671+richasriva@users.noreply.github.com>
2024-05-12 16:45:41 +0530

    Created feedback section , aligned footer and added social media links to it.

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn=document.querySelector(".btn");
+const feedback=document.querySelector(".center");
+btn.addEventListener("click",()=> {
+    center.innerHTML= `<h1>Thank You for your feedback</h1>`
+});
