68602cdefc067b2ba31547961a6f0d50e1acac48
san <120664435+krish-san@users.noreply.github.com>
2024-05-14 01:15:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -165,11 +165,5 @@ const typed = new Typed('.multiple-text',{
   loop:true,
 })
 
-//parallax effect after about section(for enhancement)
-window.addEventListener("scroll", function() {
-  var scrollTop = window.scrollY;
-  var parallaxOffset = scrollTop * 0.5;
-  var iconBoxes = document.querySelector(".icon-boxes");
-  iconBoxes.style.transform = "translateY(" + parallaxOffset + "px)";
-});
+
 
