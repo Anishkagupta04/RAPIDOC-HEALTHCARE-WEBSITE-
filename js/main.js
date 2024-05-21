ebc0f00ae66e6cc21a798dbdd16cadcaf1db1b79
Saimanjari777 <psmanjari777@gmail.com>
2024-05-21 19:34:36 +0530

    Added top to back button
    I have added smooth transition from back to top button

--- js/main.js
+++ js/main.js
@@ -138,22 +138,6 @@
     selector: '.glightbox'
   });
 
-  /**
-   * Back to top button
-   */
-   let backtotop = select('.back-to-top')
-   if (backtotop) {
-     const toggleBacktotop = () => {
-       if (window.scrollY > 100) {
-         backtotop.classList.add('active')
-       } else {
-         backtotop.classList.remove('active')
-       }
-     }
-     window.addEventListener('load', toggleBacktotop)
-     onscroll(document, toggleBacktotop)
-   }
-
 })()
 
 // typed js
